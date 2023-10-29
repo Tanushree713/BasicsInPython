685754e3c99f92e556679d8c01bec021ff04760f
Tanushree713 <123797833+Tanushree713@users.noreply.github.com>
2023-10-29 21:54:47 +0530

    findMajorityElement

--- interviewQues1.py
+++ interviewQues1.py
@@ -46,6 +46,69 @@ resultant = isCollinearPointsUsingArea(x1 , x2 ,x3 , y1 , y2 , y3)
 
 
 
+## Majority Elements ##
+## Frequency of elements present in an array is greater than n/2 , where n is length of an array ##
+nums = [1, 2 , 3, 2 , 1, 2, 2]
+
+# Approach1 :- Hash Data Structure(key:value) , Time complexity is O(n) , Space Complexity is O(n) #
+
+#importing Dictionary 
+from collections import Counter
+
+def majorityElement(nums):
+ counts = Counter(nums)
+ print(counts)
+ max_count = max(counts.values())
+ # If counts of values are same then return no one is majority element 
+ # if all(count == max_count for count in counts.values()) :
+ # return ("No one is Majority") 
+ if max_count > len(nums)//2 : 
+  return max(counts.keys() , key = counts.get)
+ else :
+    print("Not exist Majority") 
+
+result = majorityElement(nums)
+print("Majority Element is :" ,result)
+
+
+# Approach2 :-  Boyer-Moore Voting Algo , Time Complexity is O(n) , space complexity is O(1)#
+# calling 2 functions 1.findingMAjorityCandidate 2.CheckIs Majority or not
+def findMajority(nums):
+    candidate = None 
+    count = 0 
+    for num in nums :
+     if count == 0:
+        candidate = num
+     count += (1 if num == candidate else -1)
+    return candidate
+
+def isMajority(nums , candidate):
+    cnt = 0
+    n = len(nums)
+    for i in range(n):
+        if nums[i] == candidate:
+            cnt += 1
+    if cnt > n/2:
+        return 1 
+    else:
+        return 0
+
+def printMajority(nums): 
+ candidate = findMajority(nums)
+ if isMajority(nums , candidate) : 
+    print("Majority Element in given array is:" , candidate)
+ else:
+    print("Not Exist Majority Element ")    
+
+result = printMajority(nums)
+
+
+
+
+
+
+
+
 
 
 
