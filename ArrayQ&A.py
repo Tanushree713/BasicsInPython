dbba1be563a173daaca48848284355dc2e727e09
Tanushree713 <123797833+Tanushree713@users.noreply.github.com>
2023-11-05 22:35:20 +0530

    maxSubarray_Product

--- ArrayQ&A.py
+++ ArrayQ&A.py
@@ -68,44 +68,7 @@ print("product of subarray " , max_product)
 
 
 
-## 4. 3Sums
-# Getting triplets sum is equal to zero return that subarray #
-#  Two pointer Approach
-# Time Complexity is O(n^2)
-# Space Complexity is O(k) where k depneds on number of subarray 
 
-def tripletSum(nums):
-    nums.sort() # use sorting so proceed with 2 pointer approach 
-    results = []
-
-   #To confirm that we have three element atleast in an array
-    for i in range(len(nums)-2):
-        # find duplicates
-        if i > 0 and nums[i] == nums[i-1]: 
-            # skip duplicates 
-            continue
-        # start implementation of 2 pointer approach
-        left , right = i + 1 , len(nums) - 1
-        while left < right :
-            total  = nums[i] + nums[left] + nums[right]
-            if total == 0 :
-                results.append([nums[i] , nums[left] , nums[right]])
-                # removing duplicates
-                while left < right and nums[left] == nums[left+1]:
-                    left += 1
-                while left < right and nums[right] == nums[right-1]:
-                    right -= 1
-                left += 1
-                right -= 1
-            elif total > 0:
-                right -= 1
-            else:
-                left += 1
-    return results 
-
-nums = [ 1 , 2, -1 , 0 , -2 , 4 , -3]                    
-resultants = tripletSum(nums)
-print ("triplet whose sum is zero is " , resultants)
 
 
 
