4e2ce1efeabca73284a5dd822b013881462543c1
Tanushree713 <123797833+Tanushree713@users.noreply.github.com>
2024-01-23 21:00:28 +0530

    heap&hash2

--- Hashing&Heap.py
+++ Hashing&Heap.py
@@ -9,6 +9,23 @@ class Solution(object):
             return set(nums)   # unique elements
         count = Counter(nums)
         return heapq.nlargest(k , count.keys() , key=count.get)    
-        
 
 
+## 70. Kth Largest Element ##
+# Time Complexity is O() , Space Complexity is O()
+
+class Solution(object):
+    def findKthLargest(self , nums , k):
+        heap = []
+        for num in nums :
+            if len(heap)  < k :   # constraints k ,only k-elements push inside the heap 
+                heapq.heappush(heap , num)
+            else:
+                if num > heap[0] :  #smallest Ele in heap and curr Ele , if heap[0] is smaller then pop it 
+                 heapq.heappop(heap)
+                 heapq.heappush(heap , num)  # push that num ELe into heap , if num is greater 
+        return heap[0]           
+
+
+## 71. Kth Smallest  Element ##
+# Time Complexity is O() , Space Complexity is O()
