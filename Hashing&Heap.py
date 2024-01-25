4b2cb6ca3c5385deac2af5b3436ab6865c621ad6
Tanushree713 <123797833+Tanushree713@users.noreply.github.com>
2024-01-25 20:06:52 +0530

    kthClosestEle

--- Hashing&Heap.py
+++ Hashing&Heap.py
@@ -16,7 +16,7 @@ class Solution(object):
 
 class Solution(object):
     def findKthLargest(self , nums , k):
-        heap = []
+        heap = []   #minHeap
         for num in nums :
             if len(heap)  < k :   # constraints k ,only k-elements push inside the heap 
                 heapq.heappush(heap , num)
@@ -30,9 +30,9 @@ class Solution(object):
 ## 71. Kth Smallest  Element ##
 # Time Complexity is O(nlogk) , Space Complexity is O(logk)
 class Solution(object):
-    def findKthLargest(self , nums , k):
+    def findKthSmallest(self , nums , k):
 
-        heap = []
+        heap = []  #maxHeap
         for num in nums :
             if len(heap) < k :
                 heapq.heappush(heap , -num)
@@ -44,4 +44,44 @@ class Solution(object):
             return -heap[0]            
 
 
-## 72. 
+## 72. kth Closest Elements to given Value In Array ##
+# Time Complexity is O(n log k) , Space Complexity is O(log k ) #
+# Given k , x , arr ...according to x value return kth smallest elements #
+from heapq import heappush , heappop
+class Solution(object):
+    def kthClosest(self , arr , k ,x):
+        n = len(arr)
+        heap = [] # MaxHeap
+        for i in range(n):
+            heappush(heap , (abs(arr[i] - x) , arr[i]))
+        result = []
+        for i in range(k):
+            result.append(heappop(heap)[1])    
+        return  sorted(result) 
+arr = [1 , 2, 3, 4 , 5]
+k = 4        
+x = 3
+solution1 = Solution()
+result1 = solution1.kthClosest(arr , k , x)
+print("kth closest Ele " , result1)
+
+
+
+## 73. Smallest Positive Number Missing In unsorted Array ##
+# Time Complexity is O() , Space Complexity is O() #
+class Solution(object):
+    def miss_smallestPositiveNum(self, arr1 ):
+        numset = set() # hashset
+        n = len(arr1)
+        for i in range(n):
+            if arr[i] > 0 :
+                numset.add(arr[i])
+        for i in range(1 , n + 2): # We iterate up to n+2 because the smallest missing could be n+1
+            if not i in numset :
+                return i 
+arr = [ 1, 2 , 3, 4, 5]
+solution2 = Solution()
+resultant = solution2.miss_smallestPositiveNum(arr)
+print("MissedSmallPositive-Ele",resultant)
+
+
