4eeb323a28f576ce3e5ace3c4cd88d210dc5fba2
Tanushree713 <123797833+Tanushree713@users.noreply.github.com>
2024-01-29 17:02:33 +0530

    pythonLoop&Cond

--- BasicsToAdvaInPython/lecture01.py
+++ BasicsToAdvaInPython/lecture01.py
@@ -1,6 +1,6 @@
 # { * =====  IMP For Interview }
 
-##  ------  MODULES , COMMENTS , PIP ------- ##
+##  ------ 1. MODULES , COMMENTS , PIP ------- ##
 # Modules>> can be imported and used in our program 
 # PIP >> Package Manager for Python
 # COMMENTS >> used to write well mannered and understandable code.
@@ -8,7 +8,7 @@
 
 
 
-##  ------  Variables, dataTypes  -------- ##
+##  ------  2. Variables, dataTypes  -------- ##
 # Variable = data Container 
 # keyWord = reserved word eg. None , False , while , def , return  , and , break , try , import etc
 # datatype :-
@@ -26,17 +26,18 @@
 
 # Operators #
 # 1.> Arithmatic 
-# 2.> Logical
-# 3.> Assigned
-# 4.> Comparison
+# 2.> Logical (and , or , not )
+# 3.> Assigned ( = )
+# 4.> Comparison (< , > )
+# 5.> Relational( == , <= , >=  )
  
-##------------------------String----------------------------##
+##------------------------ 3. String----------------------------##
 # Escape Sequence character >> Sequence of characters after backslash eg. "\n" , "\t"
 #  friends = ["tanu" , "manu" , "harry" , "janu" ,"vanu"]
 # print(friends[0:4])  >> ["tanu" , "manu" , "harry" , "janu" ]
 # print(friends[-4])   >> manu
 
-##------------------------List And Tuples--------------------##
+##------------------------ 4. List And Tuples--------------------##
 # :Initialization of List:
 #  list = [ 1, 2 , 3 , 4]
 # tuples >> Immutable (cannot able to Update)
@@ -47,7 +48,8 @@
 # print(tuple)
 
 
-##-----------------------Dictionary And Sets----------------------##
+##-----------------------5. Dictionary And Sets----------------------##
+## Initialisation of both set and dictionary are same "{}""
 # Dictionary = collection of key value pairs 
 # Empty Dictionary #
 # a = {}        *** Empty Set 
@@ -66,5 +68,25 @@
 # set1 = { 1, 2, 3, 4 , 3, 1, 5}   >> {1, 2, 3, 4, 5}
 # b = set()          *** Empty Set 
 # print(type(b))  >> Output :- set TYPE 
-# * Set is not hashable , So not able to add any list inside set but tuples can be added 
+# *** Set is not hashable , So not able to add any list inside set but tuples can be added *** #
+# s = { 20 , 20.0 , "20"}
+# print("Length of set" , len(s)) >> 2 (BCOZ, 20 and 20.0 considered as 1-length and "20" is 2-length  )
 
+
+
+##-------------------------6. Conditional Expressions --------------------------##
+# if , elif , else are conditional Ladder 
+# Indentation have 4 Spaces or 1 Tab #
+
+##-------------------------7.LoopS --------------------------##
+# In range , 3 parameters can be passed -->> range(start, stop, step-size) where step-size is skipping the no. of elements 
+# Types > 1. For ()
+#         2. while ()
+###
+# Difference Between For and While Loop is that :
+# 1.Syntax are Different 
+# for i in range():
+# while condition :
+# 2. In For loop , know the number of iterations in advance 
+#   In while loop , not know the no. of iterations and will continued when the condition being true 
+## for-else used together,"else" will executes only when "for" runs succesfully till end ..If "break" is used inside "for" loop then, else statement cannot be executed..
