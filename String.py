6158e4eb2ff5688f6e2edf4cdc15fa94e32634db
Tanushree713 <123797833+Tanushree713@users.noreply.github.com>
2024-02-05 15:05:51 +0530

    reversedvowel

--- String.py
+++ String.py
@@ -137,4 +137,23 @@ print("Valid Anagram ? " , result  )
 
 
 
-## 38. 
+## 38. Reverse Vowels In String ##
+# Time Complexity O(n) , Space Complexity is O(n) #
+class Solution(object):
+    def reverseVowels(self , s):
+        left = 0
+        right = len(s) - 1
+        vowels = "aeiouAEIOU"
+        s =list(s)  #list are mutable , we are mutating the string values 
+        while left < right :
+            while left < right and s[left] not in vowels :
+                left += 1
+            while left < right and s[right] not in vowels:
+                right -= 1
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        return ''.join(s)
+solutions = Solution()
+result = solutions.reverseVowels("leetcode")
+print("reverse vowels in string" , result)                     
