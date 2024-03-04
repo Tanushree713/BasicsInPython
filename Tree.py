8d4f3d1cd05e0dc66c568c5cf4fa1f704abac95a
Tanushree713 <123797833+Tanushree713@users.noreply.github.com>
2024-03-04 21:18:32 +0530

    preOrdertreeTraversal

--- Tree.py
+++ Tree.py
@@ -1,6 +1,7 @@
 ## ------------------------Introduction of Tree --------------------------- ##
-# 1.Inorder Tree Traversal Algo #
-# Time Complexity is O(n) , Space Complexity is O(n)
+
+## 1.Inorder Tree Traversal Algo ##
+# Time Complexity is O(n) , Space Complexity is O(n) #
 class Node :
     def __init__(self , data):
         self.data = data
@@ -24,4 +25,28 @@ root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5)
 print("Inorder Traversal :")  
-root.inOrder()  
+# root.inOrder()  
+
+
+## 2. PreOrder Tree traversal Algo ##
+# Time Complexity is O(n) , Space Complexity is O(h) # where , h = height of binary tree
+class Node :
+    def __init__(self , data):
+        self.data = data 
+        self.left = None
+        self.right = None 
+    def preOrder(self):
+        if self:
+            print(str(self.data) + " " , end = '')
+            if self.left:
+                self.left.preOrder()
+            if self.right:
+                self.right.preOrder()  
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+print("Preorder Traversal :")  
+root.preOrder()  
+                
