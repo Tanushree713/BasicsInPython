3f9895fd1170487268b6b510376e70ea0e957c1b
Tanushree713 <123797833+Tanushree713@users.noreply.github.com>
2024-07-12 18:27:39 +0530

    Practise_66

--- PractiseSet.py
+++ PractiseSet.py
@@ -1817,8 +1817,20 @@ class MyStack :
 
 
 #66.OnlineStocks#
-# Tc is O() , Sc is O() #
+# Tc is O(n) , Sc is O(n) #
+class StockSpanner(object):
+    def __init__(self):
+        self.stack = []
+    def next(self, price):
+        span = 1
+        while self.stack and self.stack[-1][0] <= price :
+            span += self.stack[-1][1]
+            self.stack.pop()
+        self.stack.append((price , span))    
+        return span 
 
+
+#67.
 
 ##*ReverseArrayUsingStack*##
 # Time Complexity is O(2n) , Space Complexity is O(2n) #
